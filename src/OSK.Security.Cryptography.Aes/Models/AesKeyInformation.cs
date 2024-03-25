287d5fafd1a8694bf763e6adf79a5f5a3e198d48
M <mmmasent@gmail.com>
2024-03-25 02:18:04 -0400

    initial project (OSK-1)
    Motivation
    ----
    Need an initial project
    
    Modifications
    ----
    * Added project
    * Added UnitTests
    * Added github workflows
    
    Result
    ----
    Aes Cryptography implementation is now available for OSK library

---
+++ src/OSK.Security.Cryptography.Aes/Models/AesKeyInformation.cs
@@ -0,0 +1,79 @@
+﻿using OSK.Security.Cryptography;
+using OSK.Security.Cryptography.Abstractions;
+using System;
+using System.Security.Cryptography;
+
+namespace OSK.Security.Cryptography.Aes.Models
+{
+    public class AesKeyInformation : SymmetricKeyInformation
+    {
+        #region Static
+
+        public static AesKeyInformation New(int keySize, int blockSize = 128,
+            PaddingMode paddingMode = PaddingMode.PKCS7, CipherMode cipherMode = CipherMode.CBC)
+        {
+            using var aes = System.Security.Cryptography.Aes.Create();
+            CryptographicKeyHelpers.ValidateKeySize(keySize, aes.LegalKeySizes);
+
+            aes.GenerateKey();
+            aes.GenerateIV();
+            return new AesKeyInformation(aes.Key, aes.IV, blockSize, paddingMode, cipherMode);
+        }
+
+        #endregion
+
+        #region Variables
+
+        public byte[] Key { get; }
+
+        public byte[] IV { get; }
+
+        public int BlockSize { get; }
+
+        public PaddingMode PaddingMode { get; }
+
+        public CipherMode CipherMode { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public AesKeyInformation(byte[] key, byte[] iv, int blockSize,
+            PaddingMode paddingMode, CipherMode cipherMode)
+        {
+            Key = key ?? throw new ArgumentNullException(nameof(key));
+            IV = iv ?? throw new ArgumentNullException(nameof(iv));
+            BlockSize = blockSize;
+            PaddingMode = paddingMode;
+            CipherMode = cipherMode;
+        }
+
+        public AesKeyInformation(byte[] key, AesPublicKeyInformation publicKeyInformation)
+            : this(key, publicKeyInformation.IV, publicKeyInformation.BlockSize,
+                  publicKeyInformation.PaddingMode, publicKeyInformation.CipherMode)
+        {
+        }
+
+        #endregion
+
+        #region CryptographicKey Overrides
+
+        public override PublicKeyInformation GetPublicKeyInformation()
+        {
+            return new AesPublicKeyInformation()
+            {
+                BlockSize = BlockSize,
+                IV = IV,
+                CipherMode = CipherMode,
+                PaddingMode = PaddingMode
+            };
+        }
+
+        public override void Dispose()
+        {
+            Array.Clear(Key, 0, Key.Length);
+        }
+
+        #endregion
+    }
+}
---
+++ src/OSK.Security.Cryptography.Aes/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OSK.Security.Cryptography;
+using OSK.Security.Cryptography.Aes.Internal.Services;
+using OSK.Security.Cryptography.Aes.Models;
+
+namespace OSK.Security.Cryptography.Aes
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAesKeyService(this IServiceCollection services)
+        {
+            services.AddSymmetricKeyService<AesKeyService, AesKeyInformation>();
+
+            return services;
+        }
+    }
+}
